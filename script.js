1dcf482660f33b5d4694aa40b24243806a83e435
emaemilija <emazakaraite15@gmail.com>
2025-05-23 21:33:48 +0100

    jave additons
    used java to create a form application for the jobs page with checks to ensure data is valid

---
+++ script.js
@@ -0,0 +1,53 @@
+function validateForm() {
+    let isValid = true;
+
+    let fname = document.forms["myForm"]["fname"].value;
+    if (fname.trim() == "") 
+        alert("Full name must be filled out.");
+    isValid = false;
+}
+
+    let email = document.forms["myForm"]["email"].value;
+    if (email.trim() == "") {
+        alert("Email address must be filled out.");
+        isValid = false;
+    } else if (!isValidEmail(email)) {
+        alert("please enter a valid email address.");
+        isValid = false;
+    }
+
+    let phone = document.forms["myForm"]["phone"].value;
+    if (phone.trim() !== "" && !/^\d+$/.test(phone)) {
+        alert("Phone number should only contail digits.");
+        isValid = false;
+    }
+
+    let position = document.forms["myForm"]["position"].value;
+    if (position == "") {
+        alert("Please select a psoition.");
+        isValid = false;
+    }
+
+    let cvInput = document.forms["myForm"]["cv"];
+    if (cvInput.files.length === 0){
+        alert("Please upload your cv.");
+        isValid = fasle;
+    } else {
+        let cvFile = cvInput.files[0];
+        let allowedTypes = ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'];
+        if (!allowedTypes.include(cvFile.type)) {
+            alert("Only PDF, DOC, or DOCX files are allowed for CV.");
+            isValid = false;
+        }
+    }
+
+    if (!isValid) {
+        return false;
+    }
+
+    return true;
+
+    function isValidEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
